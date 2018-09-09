285995f52b0db150d24ab06868a28fb2b0c13944
Caitlin Pretorius <30002731@student.toiohomai.ac.nz>
2018-09-10 11:44:01 +1200

    not done

---
+++ Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] nums = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+               Console.WriteLine("Please enter something:");
+               Console.ReadLine();
+                                           
+            }
+            Console.WriteLine(nums[2]);
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
